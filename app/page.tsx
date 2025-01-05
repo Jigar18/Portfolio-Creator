23793c06082d3dc14c15ae8f95c040adf39b17fc
jigar <jigar1.dev@gmail.com>
2025-01-05 23:56:37 +0530

    small updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>This is going to be a website through which a user can create a portfolio website for themselves (can also be considered as a digital resume website.)</h1>
+    <div className="bg-red-500 flex justify-center items-center">
+      <main className="bg-white max-w-[90rem] h-full">
+        <h1>
+          This is going to be a website through which a user can create a
+          portfolio website for themselves (can also be considered as a digital
+          resume website.)
+        </h1>
+      </main>
     </div>
   );
 }
