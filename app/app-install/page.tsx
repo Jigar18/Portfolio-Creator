0e8cb2048c39afac3503b33d315ad1edda5e8189
jigar <jigar1.dev@gmail.com>
2025-02-22 23:07:15 +0530

    updated the repo fetching file

--- app/app-install/page.tsx
+++ app/app-install/page.tsx
@@ -1,23 +1,22 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const githubAppSlug = 'portfolio-creator';
-interface SearchParams {
-    access_token?: string;
-    login?: string;
-}
+const githubAppSlug = "portfolio-creator";
 
-export default function InstallApp({ searchParams} : {searchParams: SearchParams}) {
+export default function InstallApp() {
   const router = useRouter();
-  const { access_token, login } = searchParams;
+  const searchParams = useSearchParams();
+
+  const access_token = searchParams.get("access_token");
+  const login = searchParams.get("login");
 
   useEffect(() => {
     if (access_token && login) {
       window.location.href = `https://github.com/apps/${githubAppSlug}/installations/new`;
     } else {
-      router.push('/');
+      router.push("/");
     }
   }, [access_token, login, router]);
 
