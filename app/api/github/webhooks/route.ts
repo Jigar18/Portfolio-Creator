7c03fe295742e9cc7a9b451bd8883b4eea303c1d
jigar <jigar1.dev@gmail.com>
2025-02-22 22:27:16 +0530

    small change

--- app/api/github/webhooks/route.ts
+++ app/api/github/webhooks/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function installation(req: NextApiRequest, res: NextApiResponse) {
   const event = req.headers['x-github-event'];
 
   if (event === 'installation') {
